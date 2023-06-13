060a034fe57653b3294d3d4497eeb4dae4fe2f56
Mario Carballo Zama <afk@ellugar.co>
2023-06-12 19:43:07 -0600

    feat: add action to generate from Cataxis Google Sheet

---
+++ .github/actions/gen-from-gdocs.js
@@ -0,0 +1,82 @@
+import fs from "fs";
+import { google } from "googleapis";
+import { promisify } from "util";
+import "dotenv/config";
+
+const sheets = google.sheets("v4");
+
+const sheetID = `1qZNjZOXthLsm_NQynQ2VOPgVUMK6hfAuLeTj1HG-bV0`;
+const sheetName = `Videojuegos mexicanos`;
+const sheetRange = `A8:P744`;
+const API_KEY = process.env.GOOGLE_API_KEY;
+const request = {
+  spreadsheetId: sheetID,
+  ranges: [`${sheetName}!${sheetRange}`],
+  fields: "sheets(data(rowData(values(hyperlink,userEnteredValue))))",
+  auth: API_KEY,
+};
+
+try {
+  const response = (await sheets.spreadsheets.get(request)).data;
+  const data = response.sheets[0]?.data[0]?.rowData.map(({ values }) => {
+    const [
+      cellName,
+      cellXbox,
+      cellNull,
+      cellNintendo,
+      cellSteam,
+      cellPlayStation,
+      cellPlayStore,
+      cellAppStore,
+      cellOther,
+      cellCrowdfundingCampaign,
+      cellCrowdfundingCampaignFunded,
+      cellDevelopers,
+      cellPublishers,
+      cellDateLaunched,
+      cellGenre,
+      cellState,
+    ] = values;
+
+    return {
+      name: cellName?.userEnteredValue?.stringValue ?? null,
+      url: cellName?.hyperlink ?? null,
+      xbox: cellXbox?.hyperlink ?? null,
+      nintendo: cellNintendo?.hyperlink ?? null,
+      playStation: cellPlayStation?.hyperlink ?? null,
+      playStore: cellPlayStore?.hyperlink ?? null,
+      appStore: cellAppStore?.hyperlink ?? null,
+      other: cellOther?.hyperlink ?? null,
+      crowdfunding: {
+        url: cellCrowdfundingCampaign.hyperlink ?? null,
+        funded:
+          cellCrowdfundingCampaignFunded.userEnteredValue?.stringValue ?? null,
+      },
+      developers:
+        cellDevelopers?.userEnteredValue?.stringValue
+          ?.split(",")
+          .map((item) => item.trim()) ?? null,
+      publishers:
+        cellPublishers?.userEnteredValue?.stringValue
+          ?.split(",")
+          .map((item) => item.trim()) ?? null,
+      "date-launch": cellDateLaunched?.userEnteredValue?.numberValue ?? null,
+      genre: cellGenre?.userEnteredValue?.stringValue ?? null,
+      state: cellState?.userEnteredValue?.stringValue ?? null,
+    };
+  });
+
+  const jsonFile = {
+    games: data,
+  };
+
+  await saveFile("../../data.json", JSON.stringify(jsonFile, null, 2));
+} catch (err) {
+  console.error(err);
+}
+
+export async function saveFile(path, content) {
+  const writeFileAsync = promisify(fs.writeFile);
+  await writeFileAsync(path, content);
+  console.info(`${path} file saved`);
+}
