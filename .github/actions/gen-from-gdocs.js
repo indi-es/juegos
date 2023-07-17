e53bbbfcf8e6df3d7e7c229c8a5358753b4e6282
Mario Carballo Zama <afk@ellugar.co>
2023-07-17 16:21:35 -0600

    docs: scrap the db

--- .github/actions/gen-from-gdocs.js
+++ .github/actions/gen-from-gdocs.js
@@ -13,7 +13,7 @@ const sheets = google.sheets("v4");
 
 const sheetID = `1qZNjZOXthLsm_NQynQ2VOPgVUMK6hfAuLeTj1HG-bV0`;
 const sheetName = `Videojuegos mexicanos`;
-const sheetRange = `A9:Q1057`;
+const sheetRange = `A9:Q1060`;
 const API_KEY = process.env.GOOGLE_API_KEY;
 const request = {
   spreadsheetId: sheetID,
